83250d7ebfd51e02df075386c26bd1ef6a22c82b
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2021-06-14 14:44:26 -0300

    ISSUE #32: Creacion de Clase Mantenimiento/Repositorio

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/mantenimiento/ManteimientoRepositorio.java
@@ -0,0 +1,4 @@
+package domainapp.modules.simple.dom.mantenimiento;
+
+public class ManteimientoRepositorio {
+}
---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/mantenimiento/Mantenimiento.java
@@ -0,0 +1,6 @@
+package domainapp.modules.simple.dom.mantenimiento;
+
+public class Mantenimiento {
+
+
+}
