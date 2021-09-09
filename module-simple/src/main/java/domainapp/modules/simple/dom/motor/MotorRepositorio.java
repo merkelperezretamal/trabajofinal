95a48ae865c55a061afc27940eaee3f8aaa73823
Merkel Gonzalo <merkelgonzalo.ok@gmail.com>
2021-09-09 17:46:07 -0300

    ISSUE #25: metodo findByTipo en MotorRepositorio y agregado de funcionalidad en menubars

--- module-simple/src/main/java/domainapp/modules/simple/dom/motor/MotorRepositorio.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/motor/MotorRepositorio.java
@@ -2,10 +2,12 @@ package domainapp.modules.simple.dom.motor;
 
 import domainapp.modules.simple.dom.equipo.Equipo;
 import domainapp.modules.simple.dom.equipo.EquipoRepositorio;
+import domainapp.modules.simple.dom.equipo.QEquipo;
 import org.apache.isis.applib.annotation.*;
 import org.apache.isis.applib.services.eventbus.ActionDomainEvent;
 import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
 import org.apache.isis.applib.services.repository.RepositoryService;
+import org.datanucleus.query.typesafe.TypesafeQuery;
 
 import java.util.List;
 
@@ -25,6 +27,21 @@ public class MotorRepositorio {
     @MemberOrder(sequence = "1")
     public List<Motor> listAll() {
         return repositoryService.allInstances(Motor.class);
+    }
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT)
+    @MemberOrder(sequence = "2")
+    public List<Motor> findByTipo(
+            @ParameterLayout(named="Tipo de Modelo")
+            final ETipoModelo tipoModelo) {
+        TypesafeQuery<Motor> q = isisJdoSupport.newTypesafeQuery(Motor.class);
+        final QMotor cand = QMotor.candidate();
+        q = q.filter(
+                cand.tipoModelo.eq(q.stringParameter("tipoModelo"))
+        );
+        return q.setParameter("tipoModelo", tipoModelo)
+                .executeList();
     }
 
     public static class CreateDomainEvent extends ActionDomainEvent<MotorRepositorio> {}
