b6e3bd9a089516152d78e0daa7e4c394144dbf66
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2021-10-22 20:11:19 -0300

    ISSUE #56: edicion reportes

--- module-simple/src/main/java/domainapp/modules/simple/dom/reportes/RepoEquipos.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/reportes/RepoEquipos.java
@@ -11,14 +11,17 @@ public class RepoEquipos {
     @Getter @Setter
     private String denominacion;
     @Getter @Setter
+    private String planta;
+    @Getter @Setter
     private String motor;
     @Getter @Setter
     private String compresor;
     @Getter @Setter
     private String activo;
 
-    public RepoEquipos(String denominacion, String motor, String compresor, String activo) {
+    public RepoEquipos(String denominacion, String planta, String motor, String compresor, String activo) {
         this.denominacion = denominacion;
+        this.planta = planta;
         this.motor = motor;
         this.compresor = compresor;
         this.activo = activo;
