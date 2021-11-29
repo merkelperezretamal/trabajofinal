8bcbc4051fae7cfa94863d21e70e2fa8b71ad916
IgnacioRetamal <ignacioretamal01@gmail.com>
2021-11-29 14:09:59 -0300

    ISSUE #60: Configuracion de proyecto ionic

--- Secco-App/src/app/home/home.page.ts
+++ Secco-App/src/app/home/home.page.ts
@@ -1,38 +1,12 @@
 import { Component } from '@angular/core';
-import { AlertController, NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor(public alertController: AlertController, public navCtrl: NavController, public toastController: ToastController) {}
+  constructor() {}
 
-  async alertaLogOut() {
-    const alert = await this.alertController.create({
-      header: 'Cerrar Sesión',
-      message: '¿Esta seguro que desea cerrar sesión?',
-      buttons: [
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: (blah) => {
-            console.log('Apreto boton cancelar');
-          }
-        }, {
-          text: 'Si, salir',
-          handler: () => {
-            console.log('Apreto boton Salir');
-            this.navCtrl.navigateRoot('/login');
-          }
-        }
-      ]
-    });
-
-    await alert.present();
-  }
-  
 }
