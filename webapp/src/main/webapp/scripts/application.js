ddbf6448d635f7ae691d1c0fe7e21bd6c44bff0a
IgnacioRetamal <ignacioretamal01@gmail.com>
2021-12-10 15:33:14 -0300

    ISSUE #60: Cambio vista login y pantalla inicio

--- webapp/src/main/webapp/scripts/application.js
+++ webapp/src/main/webapp/scripts/application.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
 	/// here...
+
+	if ($("body").find(".accountManagementPanel").length > 0){
+
+        $('body').css('background-color', '#426ab2');
+        $('body').css('background-image', 'url("/images/seccoimagen.jpg")');
+        $('body').css('color', '#ffffff');
+        $('body').css('position', 'relative');
+        $('body').css('height', 'calc(100vh)');
+        $('body').css('background-size', 'cover');
+        $('body').css('background-repeat', 'no-repeat');
+        $('body').css('background-position', 'center center');
+
+        $('h4').css('margin-top', '-20px');
+
+        $("form:not(.filter) :input:visible:enabled").eq(0).attr("placeholder", "Ingrese nombre de usuario");
+        $("form:not(.filter) :input:visible:enabled").eq(1).attr("placeholder", "Ingrese contraseña");
+
+        $("img[src$='/images/Logo-principal.png']").wrap("<a href='/'> </a>");
+
+        $("button.btn[type=submit]").removeClass("btn-primary").addClass("btn-info");
+
+        $("button.btn[type=reset]").hide();
+
+    	}else{
+        $('body').css('background-color', 'white');
+        $('body').css('background-image', 'url("/images/background.jpg")');
+        $('body').css('background-repeat', 'repeat-y');
+        $('body').css('color', 'black');
+        $('body').css('position', 'relative');
+        $('body').css('height', 'calc(100vh)');
+        $('body').css('background-size', 'cover');
+        $('body').css('background-position', 'center center');
+    	}
 });
